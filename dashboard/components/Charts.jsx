0f3e87c9c0a3c5e42d87f883037217a6ff30f3fd
Ahmad-Baig-UET <ahmadbaig876@gmail.com>
2026-03-03 06:02:26 +0500

    feat: add nexus-agent and dashboard scaffold

---
+++ dashboard/components/Charts.jsx
@@ -0,0 +1,8 @@
+// TODO: Add chart components using recharts or chart.js
+export default function Charts({ data }) {
+    return (
+        <div>
+            <p>Charts will render here</p>
+        </div>
+    );
+}
---
+++ dashboard/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <nav>
+            <a href="/">Home</a>
+            <a href="/analytics">Analytics</a>
+            <a href="/marketplace">Marketplace</a>
+        </nav>
+    );
+}
